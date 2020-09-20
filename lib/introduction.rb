883b8c12954929dcf1761864cbc116e61185282b
Mike Barberry <mikebarberry@protonmail.com>
2020-09-20 19:43:30 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,3 +3,9 @@ def introduction(name)
 end
 
 introduction("Mike")
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I speak #{language}"
+end
+
+introduction_with_language("Mike", "English")
